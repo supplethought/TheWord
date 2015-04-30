4685eb754bfdd68e490099bef7c598532dcd1009
schernets <sergiy.chernets@gmail.com>
2015-04-30 16:10:42 -0700

    Bible Gateway Facade. Reformation Study Bible commentaries

--- components/theWord/theWordController.js
+++ components/theWord/theWordController.js
@@ -5,19 +5,24 @@
         function () {
             function controller($scope, $log, biblesAdapter) {
                 $scope.message = "The Word";
+                $scope.theWordGroup = {
+                    title: "The Word",
+                    content: undefined,
+                    open: false
+                };
+                $scope.readingPlansGroup = {
+                    title: "Reading Plans",
+                    content: "Dynamic Group Body - 2",
+                    open: false
+                };
+                $scope.oneAtATime = true;
                 biblesAdapter
                     .getBooks("eng-ESV")
                     .then(function (data) {
+                        $scope.theWordGroup.content = data.result;
                         $scope.books = data.result;
                         $scope.selectedBook = $scope.books[0];
                     });
-                $scope.status = {
-                    open: false,
-                    open2: false,
-                    isFirstOpen: true,
-                    isFirstDisabled: false
-                };
-                $scope.oneAtATime = true;
                 $scope.toggled = function (open) {
                     $log.log('Dropdown is now: ', open);
                 };
@@ -25,6 +30,9 @@
                     $event.preventDefault();
                     $event.stopPropagation();
                     $scope.status.isopen = !$scope.status.isopen;
+                };
+                $scope.status = {
+                    open: false
                 };
             }
 
